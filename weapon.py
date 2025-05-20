a9d8436ee1443199c2f35dcf5ba2b073c7dbd126
Midna1 <Cryaotic312279@gmail.com>
2025-05-20 18:39:38 -0400

    Update weapon.py

--- weapon.py
+++ weapon.py
@@ -30,7 +30,7 @@ ITEMS = [
     ("Hardlight Accelerator", 10, 0, 11000, 0, "all"),
     ("El-sa'ka Suppressor", 10, 0, 11000, 0, "all"),
     ("The Closer", 20, 0, 13000, 0, "all"),
-    ("Eye of the Spider", 25, 0, 13500, 0, "all"),
+    ("Eye of the Spider", 25, 0, 14000, 0, "all"),
     ("Custom Stock", 5, 0, 3750, 0, "all"),
 
     # Juno
